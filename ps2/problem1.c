2a5452675fe9ab108a28f8b42b42b33c6679dc31
Nawaz Banu <121148792+nawazbanu@users.noreply.github.com>
2023-03-16 15:57:29 +0000

    problem1.c

--- ps2/problem1.c
+++ ps2/problem1.c
@@ -9,14 +9,14 @@ The volume of the kalakand with quantity_of_milk 1.00, qunatity_of_sugar 1.00 an
 #include<math.h>
 
 void input(float *quantity_of_milk,float *quantity_of_sugar,float *weight_of_dryfruits);
-float is_volume(float quantity_of_milk,float quantity_of_sugar,float weight_of_dryfruits);
-void output(float quantity_of_milk,float quantity_of_sugar,float weight_of_dryfruits,float volume_of_kalakand){
+float find_weight(float quantity_of_milk,float quantity_of_sugar,float weight_of_dryfruits);
+void output(float quantity_of_milk,float quantity_of_sugar,float weight_of_dryfruits,float weight);
 
 int main(){
-  float quantity_of_milk, quantity_of_sugar,weight_of_dryfruits,volume_of_kalakand;
+  float quantity_of_milk, quantity_of_sugar,weight_of_dryfruits,weight;
   input(&quantity_of_milk,&quantity_of_sugar,&weight_of_dryfruits);
-  volume_of_kalakand= is_volume(quantity_of_milk, quantity_of_sugar,weight_of_dryfruits);
-  output(quantity_of_milk,quantity_of_sugar,weight_of_dryfruits,volume_of_kalakand);
+  weight = find_weight(quantity_of_milk, quantity_of_sugar,weight_of_dryfruits);
+  output(quantity_of_milk,quantity_of_sugar,weight_of_dryfruits,weight);
 }
 
 void input(float *quantity_of_milk,float *quantity_of_sugar,float *weight_of_dryfruits){
@@ -29,12 +29,12 @@ void input(float *quantity_of_milk,float *quantity_of_sugar,float *weight_of_dry
   scanf("%f",weight_of_dryfruits);
 }  
 
-float is_volume(float quantity_of_milk,float quantity_of_sugar,float weight_of_dryfruits){
-  float volume_of_kalakand;
-  volume_of_kalakand = 0.8 * sqrt(quantity_of_milk)  + quantity_of_sugar/4 + 0.8 * weight_of_dryfruits; 
-  return volume_of_kalakand;
+float find_weight(float quantity_of_milk,float quantity_of_sugar,float weight_of_dryfruits){
+  float weight;
+  weight = 0.8 * sqrt(quantity_of_milk)  + quantity_of_sugar / 4 + 0.8 * weight_of_dryfruits; 
+  return weight;
 }
 
-void output(float quantity_of_milk,float quantity_of_sugar,float weight_of_dryfruits,float volume_of_kalakand){
-  printf("volume of kalakand with quantiy_of_milk %f,quantity_of_sugar %f and weight_of_dryfruits %f is %f",quantity_of_milk,quantity_of_sugar,weight_of_dryfruits,volume_of_kalakand);
+void output(float quantity_of_milk,float quantity_of_sugar,float weight_of_dryfruits,float weight){
+  printf("volume of kalakand with quantiy_of_milk %.2f,quantity_of_sugar %.2f and weight_of_dryfruits %.2f is %.2f",quantity_of_milk,quantity_of_sugar,weight_of_dryfruits,weight);
 }
