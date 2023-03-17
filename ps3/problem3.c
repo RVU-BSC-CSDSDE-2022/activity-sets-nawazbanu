5e46799d447247328c5032435e34938e9cbdf1b4
Nawaz Banu <121148792+nawazbanu@users.noreply.github.com>
2023-03-17 09:44:39 +0000

    problem3.c

--- ps3/problem3.c
+++ ps3/problem3.c
@@ -7,3 +7,36 @@ The nth term is 8 */
 
 #include<stdio.h>
 
+int input_n();
+int find_sum(int n);
+void output(int n,int term);
+
+int main(){
+  int n,term,sum;
+  n = input_n();
+  term = find_sum(n);
+  output(n,term);
+  return 0;
+}
+
+int input_n(){
+  int n;
+  printf(" ");
+  scanf("%d",&n);
+  return n;
+}
+
+int find_sum(int n){
+  int i;
+  int a = 0,b = 1,sum = 0;
+    for(i = 1; i<= n;i++){
+    sum = a + b;
+    a = b;
+    b = sum;
+    }
+  return sum;
+}
+
+void output(int n,int term){
+  printf("The  %dth term is %d",n,term);
+}
